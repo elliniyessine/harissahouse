b365fc5bbd3a9885db3e7cff9b6eff732426011d
Yessine Ellini <yessine.ellini>
2023-05-13 12:19:13 +0100

    footer: update the link to the source code
    The previous link went private again

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,6 +1,5 @@
 <footer>
-<p>The recipes and images are <a href="https://creativecommons.org/licenses/by-sa/4.0/">cc-by-sa</a>. The <a href="https://github.com/AymenKoched/Projet-PHP">code</a> is <a href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL-3.0+</a></p>
-<!-- Either make the repo public or change the above link to a public mirror -->
+<p>The recipes and images are <a href="https://creativecommons.org/licenses/by-sa/4.0/">cc-by-sa</a>. The <a href="https://github.com/elliniyessine/harissahouse">code</a> is <a href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL-3.0+</a></p>
 </footer>
 
 <?php
